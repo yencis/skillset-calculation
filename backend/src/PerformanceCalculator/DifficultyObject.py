90bad74f4fb35aa33225f992b59b02230f262265
Albert Dong <albertd@ucla.edu>
2023-08-16 13:02:10 -0700

    Begin DifficultyObject implementation

--- backend/src/PerformanceCalculator/DifficultyObject.py
+++ backend/src/PerformanceCalculator/DifficultyObject.py
@@ -14,24 +14,27 @@ class DifficultyObject:
         index: int,
         hit_object: HitObject,
         last_object: HitObject,
-        clock_rate: float,
+        last_last_object: HitObject | None,
+        clock_rate: float,  # this is 1 for normal speed playback
     ) -> None:
         self.difficulty_objects = difficulty_objects
         self.index = index
         self.hit_object = hit_object
         self.last_object = last_object
-        self.last_last_object: HitObject | None = None
-        self.delta_time = None
-        self.start_time = None
-        self.end_time = None
-        self.strain_time = None
+        self.last_last_object = last_last_object
+        self.delta_time = (self.hit_object.time - self.last_object.time) / clock_rate
+        self.start_time = self.hit_object.time / clock_rate
+        self.end_time = None  # TODO: need get_end_time() for hitobjects
+        self.strain_time = max(self.MIN_DELTA_TIME, self.delta_time)
         self.lazy_jump_distance = None
         self.minimum_jump_distance = None
         self.minimum_jump_time = None
         self.travel_distance = None
         self.travel_time = None
         self.angle: float | None = None
-        self.hit_window_great = None
+        self.hit_window_great = (
+            None if isinstance(hit_object, Slider) else None
+        )  # TODO: need hit windows for head of slider and circles
 
     def previous(self, backwards_index: int):
         try:
@@ -46,7 +49,8 @@ class DifficultyObject:
             return None
 
     def opacity_at(self, time: float, hidden: bool):
-        pass
+        if time > self.hit_object.time:
+            return 0.0
 
     def set_distances(self, clock_rate: float):
         pass
